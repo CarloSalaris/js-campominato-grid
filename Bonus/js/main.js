bfcc148011e180d7b2347728c07d0c328166d855
CarloSalaris <carloa.salaris@gmail.com>
2023-05-09 19:16:59 +0200

    got the values from select and output to console NOT WORKING IN THE GRID

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -5,7 +5,13 @@
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; */
 
-
+/* //stabilisco dimensione quadrato in base al livello selezionato
+                if (diffLevel === 2){
+                    newSquare.style.width = ("calc(100% / 9)")
+                }else if (diffLevel === 3) {
+                    newSquare.style.width = ("calc(100% / 7)")
+                } */
+                
 
 
 
@@ -17,36 +23,22 @@
     //Seleziono il button
     const startButton = document.getElementById("startGame");
     //gli assegno l'evento "click" per fare apparire la griglia.
-    startButton.addEventListener("click",
-
+    startButton.addEventListener("click", 
         function() {
-            //Mostra griglia nascosta
-            showHiddenElement(gridElement);
-            //Creo un loop che si ripeta tante volte quante il valore massimo prestabilito
-            for (let i = 1; i <= 100; i++) {
-                //Creo una funzione per riprodurre un quadrato (ogni volta)
-                const newSquare = createGridSquare("div", "square");
-
-// Ogni cella ha un numero progressivo, da 1 a 100 (Ci saranno quindi 10 caselle per ognuna delle 10 righe.)
-                //Creo lo span che andrà in ogni quadrato
-                const newSpan = document.createElement("span");
-                //Lo appendo con il valore "i" del for loop
-                newSpan.append(i);
-                newSquare.append(newSpan);
-    
-                //associo l'evento click a ogni nuovo quadrato creato
-                newSquare.addEventListener("click",
-                    function() {
-                        // la cella cliccata si colora di azzurro
-                        this.classList.add("blue_bg");
-                        // emetto un messaggio in console con il numero della cella cliccata.
-                        console.log("the number you selected is: " + i)
-                    }
-                );
-
-                //"appendo" il quadrato
-                gridElement.append(newSquare);
-            }           
+            const diffLevel = document.getElementById("diffLevel").value;
+            console.log(diffLevel);
+            
+            if (diffLevel === 2){
+                startClickFunction(81);
+                newSquare.style.width = ("calc(100% / 9)");
+                console.log("dovrebbero essere 81!");
+                              
+            }else if (diffLevel === 3) {
+                startClickFunction(49);
+                newSquare.style.width = ("calc(100% / 7)");
+            }else {
+                startClickFunction(100);
+            }
         }
     );
 
@@ -58,11 +50,45 @@
 function showHiddenElement(elementName) {
     elementName.classList.remove("hide");
     elementName.classList.add("show");
-}
+};
 
 //Creo una funzione per riprodurre un quadrato
 function createGridSquare(tagType, classToAdd) {
     const newElement = document.createElement(tagType)
     newElement.classList.add(classToAdd);
     return newElement;
-}
+};
+
+//Funzione di click
+function startClickFunction(maxNum) {
+    //Mostra griglia nascosta
+    showHiddenElement(gridElement);
+    //Creo un loop che si ripeta tante volte quante il valore massimo prestabilito
+    for (let i = 1; i <= maxNum; i++) {
+        
+        //Creo una funzione per riprodurre un quadrato (ogni volta)
+        const newSquare = createGridSquare("div", "square");
+        
+        
+
+// Ogni cella ha un numero progressivo, da 1 a 100 (Ci saranno quindi 10 caselle per ognuna delle 10 righe.)
+        //Creo lo span che andrà in ogni quadrato
+        const newSpan = document.createElement("span");
+        //Lo appendo con il valore "i" del for loop
+        newSpan.append(i);
+        newSquare.append(newSpan);
+
+        //associo l'evento click a ogni nuovo quadrato creato
+        newSquare.addEventListener("click",
+            function() {
+                // la cella cliccata si colora di azzurro
+                this.classList.add("blue_bg");
+                // emetto un messaggio in console con il numero della cella cliccata.
+                console.log("the number you selected is: " + i)
+            }
+        );
+
+        //"appendo" il quadrato
+        gridElement.append(newSquare);
+    }           
+};
