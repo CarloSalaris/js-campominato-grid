454411aa04a7659668b7d3c7e7374c978877d460
CarloSalaris <carloa.salaris@gmail.com>
2023-05-09 17:01:22 +0200

    Macro + commenti JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+/* Consegna */
+
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+    //Seleziono il button
+    //gli assegno l'evento "click" per fare apparire la griglia.
+        //Creo un loop che siripeta tante volte quante il valore massimo prestabilito
+            //Creo una funzione per riprodurre un quadrato (ogni volta)
+
+        // Ogni cella ha un numero progressivo, da 1 a 100 (Ci saranno quindi 10 caselle per ognuna delle 10 righe.)
+            //Creo lo span che andrà in ogni quadrato
+            //Lo appendo con il valore "i" del for loop
+
+// Quando l’utente clicca su ogni cella:
+    // la cella cliccata si colora di azzurro
+    // emetto un messaggio in console con il numero della cella cliccata.
+        //associo l'evento click a ogni nuovo quadrato creato
+            //Con il click associo la classe colore
